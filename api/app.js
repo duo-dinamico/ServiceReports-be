093d8f5b6dcc78bbe0b99a03695d0c1553c19c53
Joao Carlos Silva <jcvsilva@gmail.com>
2022-01-18 19:10:42 +0000

    Add first tests.

--- api/app.js
+++ api/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const Boom = require("@hapi/boom");
+const {errorHandler} = require("./errors")
 
 const apiRouter = require("./routes/api.router");
 const docsRouter = require("./routes/docs.router");
@@ -12,6 +13,7 @@ app.use(cors());
 app.use("/api", apiRouter);
 app.use("/api-docs", docsRouter);
 
-app.all("/*", (_req, _res, next) => next(Boom.notFound("Route Not Found")));
+app.all("/*", (_req, _res, next) => next(Boom.notFound("Route Not Found.")));
+app.use(errorHandler)
 
 module.exports = app;
--- api/errors/index.js
+++ api/errors/index.js
@@ -4,3 +4,13 @@ exports.methodNotAllowed = (_req, res) => {
     const error = Boom.methodNotAllowed("That method is not allowed, please used an allowable method");
     res.status(error.output.statusCode).json(error.output.payload);
 };
+
+exports.errorHandler = (err, _req, res, next) => {
+    if (Boom.isBoom(err)) {
+        res.status(err.output.statusCode).json({...err.output.payload, data: err.data});
+    } else {
+        console.error(err);
+        const serverErr = Boom.badImplementation("terrible implementation");
+        res.status(serverErr.output.statusCode).json(serverErr.output.payload);
+    }
+};
